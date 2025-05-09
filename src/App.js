5eade434c75edce258eb9e3ac43011dca03f5426
SWMann <schaeferwmann@gmail.com>
2025-05-09 14:11:43 +0100

    Main 1

--- src/App.js
+++ src/App.js
@@ -1,55 +1,43 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useRoutes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadUser } from './redux/actions/authActions';
 
-/**
- * Uses Tailwind CSS for styling
- * Tailwind file is imported in App.css
- */
+// Components
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
 
-export default function App() {
-  return (
-    <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
-      <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img
-          className="col-span-2 row-span-3 animate-spin m-auto"
-          style={{ animationDuration: "30s" }}
-          src={logo}
-          alt="React Logo"
-          width="300"
-        />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-      </div>
+// Routes configuration
+import routes from './routes';
 
-      <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Welcome to Your New React App{" "}
-        <span className="block text-lg text-blue-400">on DigitalOcean</span>
-      </h1>
+// CSS
+import './App.css';
 
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
-        <Button
-          text="DigitalOcean Docs"
-          url="https://www.digitalocean.com/docs/app-platform"
-        />
-        <Button
-          text="DigitalOcean Dashboard"
-          url="https://cloud.digitalocean.com/apps"
-        />
-      </div>
-    </div>
-  );
-}
+// AppRoutes component to use the useRoutes hook
+const AppRoutes = () => {
+    const { isAuthenticated } = useSelector(state => state.auth);
+    return useRoutes(routes(isAuthenticated));
+};
 
-function Button({ className, text, url = "#" }) {
-  return (
-    <a
-      href={url}
-      className={`${className} py-3 px-6 bg-purple-400 hover:bg-purple-300 text-purple-800 hover:text-purple-900 block rounded text-center shadow flex items-center justify-center leading-snug text-xs transition ease-in duration-150`}
-    >
-      {text}
-    </a>
-  );
-}
+const App = () => {
+    const dispatch = useDispatch();
+
+    // Load user on initial render if authenticated
+    useEffect(() => {
+        dispatch(loadUser());
+    }, [dispatch]);
+
+    return (
+        <Router>
+            <div className="app-container">
+                <Header />
+                <main className="main-content">
+                    <AppRoutes />
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
+
+export default App;
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -12,6 +11,4 @@ root.render(
             <App />
         </Provider>
     </React.StrictMode>
-
 );
-serviceWorker.unregister();
